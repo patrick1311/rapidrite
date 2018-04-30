9a2b31316c421cd7229b9d90ed8c8b418bcc1fb3
Patrick Quach <patrickquach96@gmail.com>
2018-04-30 15:13:36 -0700

    Updates

---
+++ header.js
@@ -0,0 +1,15 @@
+document.write(
+		'<div id="logo"><p>insert logo img here</p></div>' +
+    	'<div id="acc_info">' +
+    		'<a href="signup.html" id="signup">Sign Up</a>' +
+    		'<a href="login.html" id="login">Log In</a>' +
+    	'</div>' +
+		'<nav>' +   		
+			'<a href="homepage.html">Home</a>' +
+    		'<a href="#news.html">News</a>' +
+    		'<a href="#comics.html">Comics</a>' +
+			'<a href="#account.html">Account</a>' +    	
+			'<a href="#about.html">About</a>' +
+    	'</nav>'
+    	);
+
